906b1e7b0851808067c6c777f3cc85792f9220bc
Mohit Deshpande <mohitd2000@gmail.com>
2017-11-27 13:31:57 -0500

    Implemented multi-scale covariance tracking

---
+++ covTracking.m
@@ -0,0 +1,59 @@
+function [box] = covTracking(searchRegion,  cov, initialSize, K)
+%UNTITLED Summary of this function goes here
+%   searchRegion: MxNx3 region to search for best cov matching
+%   cov: model covariance matrix
+%   initialSize: [height width] of window
+%   K: Qx1 scale initial size
+
+boxes = [];
+scores = [];
+[height, width, ~] = size(searchRegion);
+for i = 1:2
+    for k = 1:size(K,1)
+        windowSize = floor(K(k) .* initialSize);
+        bestDistance = Inf;
+        bestMatch = zeros(4);
+
+        for r = 1:(height - windowSize(1) + 1)
+            for c = 1:(width - windowSize(2) + 1)
+                roi = searchRegion(r:(r+windowSize(1)-1), c:(c+windowSize(2)-1), :);
+
+                roiCov = computeCovMatrix(roi);
+                distance = covMetric(cov, roiCov);
+
+                if distance < bestDistance
+                    bestDistance = distance;
+                    bestMatch = [c r windowSize(2) windowSize(1)];
+                end
+
+            end
+        end
+        
+        if bestDistance == Inf
+            continue
+        end
+        
+        scores = [scores; bestDistance];
+        boxes = [boxes; bestMatch];
+    end
+    
+    % flip aspect ratio for next iteration
+    initialSize = [initialSize(2) initialSize(1)];
+end
+
+% flip for NMS
+scores = scores .* -1;
+
+if size(boxes, 1) > 0
+    [box,scores] = selectStrongestBbox(boxes, scores, 'OverlapThreshold', 0.05);
+    
+    % select box with largest score (smallest distance)
+    [~, idx] = max(scores);
+    box = box(idx, :);
+    
+else
+    box = [0 0 0 0];
+end
+
+end
+
--- displayImages.m
+++ displayImages.m
@@ -1,4 +1,4 @@
-function displayImages(c0, c1, c0_p, c1_p)
+function displayImages(c0, c1, c0_p, extents, c1_p, match, searchRegion)
 %Function displays the 2 images in a subplot with corresponding points
 %   c0: image 1
 %   c1: image 2
@@ -8,12 +8,21 @@ function displayImages(c0, c1, c0_p, c1_p)
 subplot(1, 2, 1), subimage(c0);
 hold on
 plot(c0_p(1), c0_p(2), 'g*'),
+rectangle('Position',[c0_p-extents 2.*extents],...
+          'EdgeColor', 'r',...
+          'LineWidth',1);
 hold off
 subplot(1, 2, 2), subimage(c1);
 hold on
 %Check if calculated point is located in image
 if((c1_p(1) > 0 && c1_p(2) > 0) && (c1_p(1) <= size(c1, 2) && c1_p(2) <= size(c1, 1)))
-    plot(c1_p(1), c1_p(2), 'g*');
+    %plot(c1_p(1), c1_p(2), 'g*');
+    rectangle('Position',[searchRegion(1,:) searchRegion(2,:) - searchRegion(1,:)],...
+          'EdgeColor', 'b',...
+          'LineWidth',1);
+    rectangle('Position',match,...
+          'EdgeColor', 'r',...
+          'LineWidth',1);
 else
     plot(size(c1, 2) / 2, size(c1, 1) / 2, 'rx', 'MarkerSize', 65);
 end
